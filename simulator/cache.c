cfee7738e1b185e42366ef3c6558824050127c2a
Om Deshmukh <od66@cornell.edu>
2022-11-09 16:21:09 -0500

    Set up project structure

---
+++ simulator/cache.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "cache.h"
+#include "print_helpers.h"
+
+cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t protocol, bool lru_on_invalidate_f){
+  cache_t *cache = malloc(sizeof(cache_t));
+  cache->stats = make_cache_stats();
+  
+  cache->capacity = capacity;      // in Bytes
+  cache->block_size = block_size;  // in Bytes
+  cache->assoc = assoc;            // 1, 2, 3... etc.
+
+  // Implementation details
+  // Calculate cache parameters Calculate parameters
+  // 
+  cache->n_cache_line = 1;
+  cache->n_set = 1;
+  cache->n_offset_bit = 1;
+  cache->n_index_bit = 1;
+  cache->n_tag_bit = 1;
+
+  // Create the cache lines and the array of LRU bits
+  // 
+  // 
+  // Implementation details
+
+  cache->lines = NULL;
+  cache->lru_way = NULL;
+
+  // Initialize cache tags to 0, dirty bits to false,
+  // state to INVALID, and LRU bits to 0
+  // Implementation details
+  for (int i = 0; i < 1; i++) {
+    for (int j = 0; j < 1; j++) {
+      // body goes here
+    }
+  }
+
+  cache->protocol = protocol;
+  cache->lru_on_invalidate_f = lru_on_invalidate_f;
+  
+  return cache;
+}
+
+/* Given a configured cache, returns the tag portion of the given address.
+ *
+ * Example: a cache with 4 bits each in tag, index, offset
+ * in binary -- get_cache_tag(0b111101010001) returns 0b1111
+ * in decimal -- get_cache_tag(3921) returns 15 
+ */
+unsigned long get_cache_tag(cache_t *cache, unsigned long addr) {
+  // Implementation details
+  return 0;
+}
+
+/* Given a configured cache, returns the index portion of the given address.
+ *
+ * Example: a cache with 4 bits each in tag, index, offset
+ * in binary -- get_cache_index(0b111101010001) returns 0b0101
+ * in decimal -- get_cache_index(3921) returns 5
+ */
+unsigned long get_cache_index(cache_t *cache, unsigned long addr) {
+  // Implementation details
+  return 0;
+}
+
+/* Given a configured cache, returns the given address with the offset bits zeroed out.
+ *
+ * Example: a cache with 4 bits each in tag, index, offset
+ * in binary -- get_cache_block_addr(0b111101010001) returns 0b111101010000
+ * in decimal -- get_cache_block_addr(3921) returns 3920
+ */
+unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr) {
+  // Implementation details
+  return 0;
+}
+
+
+/* this method takes a cache, an address, and an action
+ * it proceses the cache access. functionality in no particular order: 
+ *   - look up the address in the cache, determine if hit or miss
+ *   - update the LRU_way, cacheTags, state, dirty flags if necessary
+ *   - update the cache statistics (call update_stats)
+ * return true if there was a hit, false if there was a miss
+ * Use the "get" helper functions above. They make your life easier.
+ */
+bool access_cache(cache_t *cache, unsigned long addr, enum action_t action) {
+  // Implementation details
+
+
+  return true;  // cache hit should return true
+}
